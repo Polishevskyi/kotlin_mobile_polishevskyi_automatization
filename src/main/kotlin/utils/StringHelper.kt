01c338df0516425e7fc013ded11c14143d495ca9
Serhii Polishevskyi <serhii.polishevskyi@welltech.com>
2024-09-19 20:58:01 +0300

    Small refactoring

--- src/main/kotlin/utils/StringHelper.kt
+++ src/main/kotlin/utils/StringHelper.kt
@@ -7,10 +7,9 @@ object StringHelper {
     const val ERROR_PASSWORD_REQUIRED = "Password is required"
     const val ERROR_INVALID_CREDENTIALS = "Provided credentials do not match any user in this service."
     const val DIGIT_ONE = "1"
-    const val ITEM_PRICE = "\$29.99"
+    const val ITEM_PRICE = "$29.99"
     const val PRODUCT_NAME_ASCENDING = "Sauce Labs Backpack"
     const val PRODUCT_NAME_DESCENDING = "Test.allTheThings() T-Shirt"
     const val PRODUCT_PRICE_ASCENDING = "Sauce Labs Onesie"
     const val PRODUCT_PRICE_DESCENDING = "Sauce Labs Fleece Jacket"
-    const val HEADER_APP = "Products"
 }
